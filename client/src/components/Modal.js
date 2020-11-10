cb877ccc1dc8f6c64a12cb04a60939cdcf8714f7
agusdanis <agusdani@gmail.com>
2020-11-10 17:10:52 +0700

    Modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import history from "../history";
 
-const Modal = props => {
-return <ReactDOM.createPortal(
-    <div></div>
-)
-}
+const Modal = (props) => {
+  //   console.log("Modal: ", props);
+  return ReactDOM.createPortal(
+    <div
+      onClick={props.onDismiss}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">
+          {/* <p>Are you sure you want to delete this stream ?</p> */}
+          {props.content}
+        </div>
+        <div className="actions">
+          {props.actions}
+          {/* <button className="ui button red">Delete</button> */}
+          {/* <button className="ui button">Cancel</button> */}
+        </div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
 
 export default Modal;
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  const actions = (
+    <>
+      <button className="ui button red">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure want to delete this stream ?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
